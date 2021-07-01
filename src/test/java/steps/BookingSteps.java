084c9d0afc1bf11c6930680382ac2b31594ed145
Valiantsin_Chesnakou <valiantsin.chesnakou@gmail.com>
2021-07-01 19:24:52 +0300

    Enhanced steps

--- src/test/java/steps/BookingSteps.java
+++ src/test/java/steps/BookingSteps.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import static com.codeborne.selenide.Selenide.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasToString;
 
 
 public class BookingSteps {
@@ -25,10 +26,21 @@ public class BookingSteps {
         $(".sb-searchbox__button").click();
     }
 
-    @Then("Hotel {string} should be on the Search results page")
-    public void hotelApartmentShouldBeOnTheSearchResultsPage(String hotel) {
-        $(By.xpath("//span[contains(text(),'Show prices')]")).shouldBe(Condition.visible);
-        assertThat($$(".sr-hotel__name").texts(), hasItem(hotel));
+    @Then("Hotel {string} should be on the {string} Search results page")
+    public void hotelApartmentShouldBeOnTheSearchResultsPage(String hotel, String isFirstLine) {
+        if (isFirstLine.equals("")) {
+            $(By.xpath("//span[contains(text(),'Show prices')]")).shouldBe(Condition.visible);
+            assertThat($$(".sr-hotel__name").texts(), hasItem(hotel));
+        } else if (isFirstLine.equals("first line")){
+            $(By.xpath("//span[contains(text(),'Show prices')]")).shouldBe(Condition.visible);
+            assertThat($(".sr-hotel__name").getText(), hasToString("hotel"));
+        } else {
+            try {
+                throw new Exception("");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Given("User is looking for hotels in {string} city")
--- src/test/resources/features/booking.feature
+++ src/test/resources/features/booking.feature
@@ -6,3 +6,31 @@ Feature: Search on Booking.com
     Then Hotel 'Hostel Urban' should be on the Search results page
     #To implement this step:
     #Then Hotel 'Hostel Urban' rating is 9.4
+
+  Scenario Outline: Search by city
+    Given User is looking for hotels in "<City>" city
+    When User does search
+    Then Hotel '<HotelName>' should be on the "first line" Search results page
+    Then Hotel '<HotelName>' should be on the first line of the Search results page
+    #To implement this step:
+    #Then Hotel 'Hostel Urban' rating is 9.4
+
+    Examples:
+      | City  | HotelName               |
+      | Minsk | Hostel Urban            |
+      | Brest | Hampton by Hilton Brest |
+      | Gomel | AMAKS Vizit Hotel       |
+
+  Scenario Outline: Search by city
+    Given User is looking for hotels in "<City>" city
+    When User does search
+    Then Hotel '<HotelName>' should be on the "first line" Search results page
+    Then Hotel '<HotelName>' should be on the first line of the Search results page
+    #To implement this step:
+    Then Hotel 'Hostel Urban' rating is 9.4
+
+    Examples:
+      | City  | HotelName               |
+      | Minsk | Hostel Urban            |
+      | Brest | Hampton by Hilton Brest |
+      | Gomel | AMAKS Vizit Hotel       |
